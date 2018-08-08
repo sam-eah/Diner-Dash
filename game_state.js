9448036725961e7503f7f96db6ea40b69037cb73
Sam <s.sam25@laposte.net>
2018-08-08 14:21:12 +0200

    update

--- game_state.js
+++ game_state.js
@@ -1,88 +1,46 @@
-class game_state extends Phaser.Scene {
-    constructor() {
-        super({key:"game_state"});
-        this.centerX = 1920 / 2;
-        this.centerY = 1080 / 2;
-        this.speed = 500;
-        this.scale = 0.3;
-        this.tolerance = 10;
-    }
-    
-    preload() {
+var demo = {};
+demo.game_state = function () {};
+demo.game_state.prototype = {
+    centerX: 1920 / 2,
+    centerY: 1080 / 2,
+    speed: 500,
+    wscale: 0.3,
+    tolerance: 10,
+//    image_path: '',
+//    with_food: false,
+//    direction: 2,
+    preload: function () {
         this.load.image('background', 'assets/backgrounds/game_play_bg.png');
         this.load.image('destination', 'assets/sprites/destination.png');
         
-        this.load.spritesheet('waitress_back', 
-            'assets/spitesheets/waitress/back.png',
-            { frameWidth: 300, frameHeight: 775 }
-        );
-        this.load.spritesheet('waitress_front', 
-            'assets/spitesheets/waitress/front.png',
-            { frameWidth: 300, frameHeight: 775 }
-        );
-        this.load.spritesheet('waitress_ideal', 
-            'assets/spitesheets/waitress/ideal.png',
-            { frameWidth: 300, frameHeight: 775 }
-        );
-        this.load.spritesheet('waitress_side', 
-            'assets/spitesheets/waitress/side.png',
-            { frameWidth: 512, frameHeight: 775 }
-        );
-    }
-    
-    create(){
-        this.background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+        this.load.spritesheet('waitress_back',
+                              'assets/spitesheets/waitress/back.png', 300, 775);
+        this.load.spritesheet('waitress_front',
+                              'assets/spitesheets/waitress/front.png', 300, 775);
+        this.load.spritesheet('waitress_ideal',
+                              'assets/spitesheets/waitress/ideal.png', 300, 775);
+        this.load.spritesheet('waitress_side',
+                              'assets/spitesheets/waitress/side.png', 512, 775);
         
-        this.waitress = this.physics.add.sprite(this.centerX, this.centerY, 'waitress_ideal');
-        this.waitress.setScale(this.scale);
-        this.waitress.setOrigin(0.5, 1);
-        this.waitress.setCollideWorldBounds(true);
+    },
+    create: function () {
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
         
-        this.anims.create({
-            key: 'back',
-            frames: this.anims.generateFrameNumbers('waitress_back', { start: 0, end: 18 }),
-            frameRate: 20,
-            repeat: -1
-        });
+        this.add.sprite(0, 0, 'background');
+        this.waitress = this.add.sprite(this.centerX, this.centerY, 'waitress_ideal');
+        this.waitress.anchor.setTo(0.5, 0.5);
+        this.waitress.scale.setTo(this.wscale); 
+        this.waitress.frame = 1;
+        
+        this.foo = [];
+        for (var i = 0; i <= 17; i++) {
+           this.foo.push(i);
+        }
 
-        this.anims.create({
-            key: 'front',
-            frames: this.anims.generateFrameNumbers('waitress_front', { start: 0, end: 18 }),
-            frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'ideal',
-            frames: this.anims.generateFrameNumbers('waitress_ideal', { start: 0, end: 18 }),
-            frameRate: 10,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: 'side',
-            frames: this.anims.generateFrameNumbers('waitress_side', { start: 0, end: 18 }),
-            frameRate: 10,
-            repeat: -1
-        });
-        
-        this.key_Z = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
-        this.key_Q = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
-        this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        
-        this.cursors = this.input.keyboard.createCursorKeys();
-          
-        this.input.keyboard.on('keyup', function(e){
-            if (e.keyCode == 32) {
-                this.scene.launch("pause_state");
-                this.scene.pause();
-
-            }
-        }, this);
-    }
-    
-    update(){
-        
+        this.waitress.animations.add('ideal', this.foo, 10, true);
+        this.waitress.animations.play('ideal');
+    },
+    update: function () {
         this.setDestination = function(x, y) {
             var dx = x - this.waitress.x;
             var dy = y - this.waitress.y;
@@ -91,7 +49,7 @@ class game_state extends Phaser.Scene {
             var vy = this.speed*dy/d;
             
             if (this.img) this.img.destroy();
-            this.img = this.add.image(x, y, 'destination').setOrigin(0.5, 0.5);
+            this.img = this.add.sprite(x, y, 'destination').anchor.setTo(0.5, 0.5);
             this.destination =  {
                 x: x,
                 y: y,
@@ -108,38 +66,37 @@ class game_state extends Phaser.Scene {
             
         }
         
-        if (this.cursors.left.isDown){
-            this.setDestination(this.waitress.x - 2 * this.tolerance, this.waitress.y);
-        } else if (this.cursors.right.isDown){
-            this.setDestination(this.waitress.x + 2 * this.tolerance, this.waitress.y);
-        } else if (this.cursors.up.isDown) {
-            this.setDestination(this.waitress.x, this.waitress.y - 2 * this.tolerance);
-        } else if (this.cursors.down.isDown) {
-            this.setDestination(this.waitress.x, this.waitress.y + 2 * this.tolerance);
-        }
+//        if (this.cursors.left.isDown){
+//            this.setDestination(this.waitress.x - 2 * this.tolerance, this.waitress.y);
+//        } else if (this.cursors.right.isDown){
+//            this.setDestination(this.waitress.x + 2 * this.tolerance, this.waitress.y);
+//        } else if (this.cursors.up.isDown) {
+//            this.setDestination(this.waitress.x, this.waitress.y - 2 * this.tolerance);
+//        } else if (this.cursors.down.isDown) {
+//            this.setDestination(this.waitress.x, this.waitress.y + 2 * this.tolerance);
+//        }
         
         this.moveLeft = function(){
-            this.waitress.anims.play('side', true);
-            this.waitress.setScale(- this.scale, this.scale); 
+        this.waitress.loadTexture('waitress_side', 0, false);
+            this.waitress.scale.setTo(- this.scale, this.scale); 
         }
         this.moveRight = function() {
-            this.waitress.anims.play('side', true);
-            this.waitress.setScale(this.scale);
+        this.waitress.loadTexture('waitress_side', 0, false);
+            this.waitress.scale.setTo(this.scale);
         }
         this.moveUp = function() {
-            this.waitress.anims.play('back', true);
+        this.waitress.loadTexture('waitress_back', 0, false);
         } 
         this.moveDown = function () {
-            this.waitress.anims.play('front', true);
+        this.waitress.loadTexture('waitress_up', 0, false);
         }
         this.stop = function () {
-            this.waitress.anims.play('ideal', true);
+        this.waitress.loadTexture('waitress_ideal', 0, false);
         }
         
-        this.input.on('pointerdown', function(event) {
-            this.stop();
-            this.setDestination(this.input.x, this.input.y);            
-        }, this);
+        this.input.onDown.addOnce(this.clicked, this);
+        
+
         
         
         if (this.destination) {
@@ -168,5 +125,11 @@ class game_state extends Phaser.Scene {
             
 
         }
+    },
+    clicked: function() {
+        this.stop();
+        this.setDestination(this.input.x, this.input.y);   
+
     }
-}
+};
+
